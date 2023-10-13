4aab8937024d8c1e8950d8a99a567870c4c11f89
Juan C Lopez <juancalpz@yahoo.com>
2023-10-13 13:56:06 -0400

    C Basics error messages...

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - Print "message" to standard error.
+ * main - Prints "message" to standard error message
  *
- * Return Always (1)
+ * Return 1 Always
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
