e875015d738d335cfd466690e98a5aed61d2b5f5
Juan C Lopez <juancalpz@yahoo.com>
2023-10-16 11:11:50 -0400

    C Last digit of int...

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	d = n % 10;
-	printf("Last digit of,  %d, n,  is ");
+	printf("Last digit of %d is %d", n, d);
 	if (d > 5)
 	{
 		printf("%d, d,  and is greater than 5\n");
