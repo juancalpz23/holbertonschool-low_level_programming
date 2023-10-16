cddd93f3c4f09a44c00332822efe0b59389177a1
Juan C Lopez <juancalpz@yahoo.com>
2023-10-16 11:02:15 -0400

    C Last digit of int...

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints the last digit of n and useses else-if statement to compare
+ *
+ * Return: 0 is Success
+ **/
+int main(void)
+{
+	int n, d;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	d = n % 10;
+	printf("Last digit of, " "%d", n, " is ");
+	if (d > 5)
+	{
+		printf("%d", d, " and is greater than 5\n");
+	}
+	else{
+		if(d == 0)
+		{
+			printf("%d", d, " and is 0\n");
+		}
+		if(d < 6 && d != 0)
+		{
+			printf("%d", d, " and is less than 5 and not 0\n");
+		}
+	}
+	return (0);
+}
