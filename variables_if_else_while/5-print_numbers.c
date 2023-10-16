1c8d36a7b3b5e45728e2bf591a7a4486fe766db8
Juan C Lopez <juancalpz@yahoo.com>
2023-10-16 14:10:36 -0400

    Print numbers 0 - 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints single digit numbers up to 10
+ *
+ *Return: 0 Success
+ **/
+int main(void)
+{
+	int x;
+
+	for (x == 0; x <= 10; x++)
+	{
+		printf("d", x, "\n");
+	}
+	return (0);
+}
