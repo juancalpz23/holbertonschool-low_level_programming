51c2e1957d83a461680de53a23aaefd900158b8c
Juan C Lopez <juancalpz@yahoo.com>
2023-10-16 14:25:43 -0400

    C 0-9 using putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Print 0 - 9 using putchar
+ *
+ * Return: 0 Success
+ **/
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
