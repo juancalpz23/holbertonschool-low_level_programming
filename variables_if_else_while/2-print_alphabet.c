28e5bdbd5bd77a5b90d2cabdac8aae043fde22df
Juan C Lopez <juancalpz@yahoo.com>
2023-10-16 13:09:09 -0400

    C putchar for...

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -  Print the alphabet in lower case
+ *
+ * Return: 0 Success
+ **/
+int main(void)
+{
+	char a;
+	for(a = "a" ; a <= "z" ; a++);
+	{
+		putchar(a);
+	}
+	putchar("\n");
+	return (0);
+}
