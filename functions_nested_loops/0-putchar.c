41b38c631a0c1bab26f87fa4b2b4a1fdb9e18b2e
Juan C Lopez <juancalpz@yahoo.com>
2023-10-17 10:14:48 -0400

    Print _putchar\n

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Print "_putchar" followed by a new line
+ *
+ * Result: 0 Success 1 Error
+ **/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
