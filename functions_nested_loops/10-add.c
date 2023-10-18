870adf2d6d3abbd93a8971b226c96f90b273e9a3
Juan C Lopez <juancalpz@yahoo.com>
2023-10-18 13:28:58 -0400

    C add function

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Adds two integer and returns the result
+ *@x: variable to be added
+ *@y: other variable to be added
+ * Return: result 'z'  Success
+ **/
+int add(int x, int y)
+{
+	int z = x + y;
+
+	return (z);
+}
