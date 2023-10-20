6e0d42eda4643c22db1e518839be4fe5855642a4
Juan C Lopez <juancalpz@yahoo.com>
2023-10-20 08:59:18 -0400

    C multiplies integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - Multiplies the 2 given variables
+ *
+ * @a: firt given variable to be multiplied
+ * @b: second given variable to be multiplied
+ *
+ * Return: 0 Success
+ **/
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	_putchar(c);
+	return (0);
+}
