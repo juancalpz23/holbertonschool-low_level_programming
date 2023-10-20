a016c4e892d043172ca2496a6eccda6a1681dfd6
Juan C Lopez <juancalpz@yahoo.com>
2023-10-20 11:19:29 -0400

    C - Prints most numbers except 2 and 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,16 @@ void print_most_numbers(void)
 {
 	int x;
 
-	for (x = 0; x <= 0; x++)
+	for (x = 0; x <= 9; x++)
 	{
 		if (x == 2 || x == 4)
 		{
 			continue;
 		}
-		_putchar(x + '0');
+		else
+		{
+			_putchar(x + '0');
+		}
 	}
+	_putchar('\n');
 }
