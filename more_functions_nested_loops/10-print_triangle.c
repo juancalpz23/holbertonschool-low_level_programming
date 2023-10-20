0cb818cd01caf17f55f0f22dddbe3e3cbc89197c
Juan C Lopez <juancalpz@yahoo.com>
2023-10-20 15:33:17 -0400

    C TRiangle Square

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,31 +1,32 @@
 #include "main.h"
 /**
- * print_triangle - Prints a triangle square
- * @size: The size of the triangle
+ * print_triangle - function to print symbols in triangle
+ *
+ * @size : integer value
+ *
  * Return: N/A
  **/
 void print_triangle(int size)
 {
-	int x, y, z;
-
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (x = 0; x < size; x++)
+		int x, y;
+
+		for (x = 1; x <= size; x++)
 		{
-			for (y = size - x; y > 1; y--)
+			for (y = x; y < size; y++)
 			{
-				_putchar(32);
+				_putchar(' ');
 			}
 
-			for (z = 0; z <= x; z++)
+			for (y = 1; y <= x; y++)
 			{
-				_putchar(35);
+				_putchar('#');
 			}
-
 			_putchar('\n');
 		}
 	}
