7256b1cf7f9f58f5914e65ab90984fbfa8b36085
Juan C Lopez <juancalpz@yahoo.com>
2023-10-23 11:23:02 -0400

    C strlen prototype function

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - Retruns the length of a string
+ *
+ * @s: String to determine its length
+ *
+ * Return: String Length
+ **/
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0; s[length] != '\0'; length++)
+	return (length);
+}
