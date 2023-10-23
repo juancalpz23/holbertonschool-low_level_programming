ded8543e39fde9e82c02ef73972aeca50bf72d93
Juan C Lopez <juancalpz@yahoo.com>
2023-10-23 11:52:33 -0400

    C string length

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * _strlen - Retruns the length of a string
+ * _strlen - Returns a strings length
  *
- * @s: String to determine its length
+ * @s: String to determine length
  *
- * Return: String Length
+ * Return: S Length
  **/
 int _strlen(char *s)
 {
 	int length;
 
 	for (length = 0; s[length] != '\0'; length++)
+		;
 	return (length);
 }
