ecb4480025c4c47828b1a89e40f00a411e25d072
Juan C Lopez <juancalpz@yahoo.com>
2023-10-23 10:08:23 -0400

    C Updates Pointer to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - Takes a pointer and updates its value to 98
+ *
+ *@n: Value to be changed
+ *
+ *Return: 0 Success
+**/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
