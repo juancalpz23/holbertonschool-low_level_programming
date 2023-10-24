4fd15fd3dc89e5f8e65f164b0327e3d0775e07d7
Juan C Lopez <juancalpz@yahoo.com>
2023-10-23 20:25:28 -0400

    C Prints n number of elements in an array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - Prints a given number of elements in
+ * an array
+ *
+ * @a: Pointer variable
+ * @n: Number of elements to be printed
+ *
+ **/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	putchar('\n');
+}
