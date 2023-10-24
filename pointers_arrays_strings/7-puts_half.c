a010eaebe7bd3504661da41b50011887cb6388b5
Juan C Lopez <juancalpz@yahoo.com>
2023-10-23 20:01:54 -0400

    C Prints half the string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half - Prints half of a string
+ *
+ * @str: Input string
+ **/
+void puts_half(char *str)
+{
+	int i, h;
+
+	i = 0;
+	while (str[i] != '0')
+		i++;
+
+	h = i / 2;
+
+	if (i % 2 == 1)
+		h++;
+
+	while (h < i)
+	{
+		_putchar (str[h]);
+		h++;
+	}
+	_putchar('\n');
+}
