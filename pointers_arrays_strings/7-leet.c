dbcad2a5a9e4a5769f009ddf56b8d1f9a1d355a4
Juan C Lopez <juancalpz@yahoo.com>
2023-10-25 16:50:40 -0400

    C Encode using 1337

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* leet - Encode using 1337
+*
+*@str:string that will be encoded
+
+*Return:encoded string
+*/
+
+char *leet(char *str)
+{
+	int index1 = 0, index2;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (str[++index1])
+	{
+		for (index2 = 0; index2 <= 7; index2++)
+		{
+			if (str[index1] == leet[index2] ||
+			 str[index1] - 32 == leet[index2])
+				str[index1] = index2 + '0';
+		}
+	}
+	return (str);
+}
