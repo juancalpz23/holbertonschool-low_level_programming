c34d6d6df648c73e74678c1fc037a5f5ec94baa9
Juan C Lopez <juancalpz@yahoo.com>
2023-10-26 14:52:05 -0400

    Locates a string

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strstr - Locates a substring
+ *
+ * @haystack: Array to be searched
+ * @needle: String we are looking for
+ *
+ * Return: string pointer or null
+ **/
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		char *temp;
+
+		temp = haystack;
+
+		while (*needle == *haystack)
+		{
+			haystack++, needle++;
+		}
+		if (*needle == '\0')
+			return (temp);
+		haystack++;
+	}
+	return (0);
+}
