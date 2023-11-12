7863949f5f9529dcb01b1d0a15a6374f966020d1
Juan C Lopez <juancalpz@yahoo.com>
2023-11-12 10:32:49 -0400

    Betty fix

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 #include "3-calc.h"
 /**
- * get_op_func - pointer to function 
+ * get_op_func - pointer to function
  *
  * @s: operator passed as argument
  *
@@ -11,22 +11,22 @@ int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
 	{"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
-
-    while (i < 5)
-    {
-	    if (*(ops[i].op) == *s && s[0] != '\0')
-	    {
-		    return (ops[i].f);
-	    }
-	    i++;
-    }
-    i = 0;
-    return (0);
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	int i;
+	
+	while (i < 5)
+	{
+		if (*(ops[i].op) == *s && s[0] != '\0')
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	i = 0;
+	return (0);
 }
