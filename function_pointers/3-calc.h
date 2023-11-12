8f8f5ef53fdf759f31fa30484628cb859cd6198b
Juan C Lopez <juancalpz@yahoo.com>
2023-11-12 10:38:17 -0400

    Betty fix

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -8,12 +8,9 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
-
-#include <stdlib.h>
-#include <stdio.h>
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
