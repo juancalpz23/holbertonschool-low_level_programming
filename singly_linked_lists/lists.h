23e2d32f1fda7acdd5c93b196e9e05b66a95063a
Juan C Lopez <juancalpz@yahoo.com>
2023-11-28 10:45:32 -0400

    Betty fix

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,3 +1,5 @@
+#ifndef LIST_H
+#define LIST_H
 #include <stdio.h>
 #include <unistd.h>
 /**
@@ -10,9 +12,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 int _putchar(char c);
@@ -21,3 +23,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
+#endif
