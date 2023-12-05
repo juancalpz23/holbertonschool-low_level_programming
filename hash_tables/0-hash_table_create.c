b0b91693b79636887d9db627554e53837b4633d2
Juan C Lopez <juancalpz@yahoo.com>
2023-12-05 10:10:34 -0400

    added 0-hash_table_create.c file

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a new hash table
+ * @size: size of the hash table
+ * Return: pointer
+ **/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new = NULL;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	new = malloc(sizeof(hash_table_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->size = size;
+	new->array = malloc(sizeof(hash_node_t *) * size);
+	if (new->array == NULL)
+	{
+		return (NULL);
+	}
+	return (new);
+}
